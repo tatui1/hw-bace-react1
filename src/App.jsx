de2a76b7e4125be48d7507f778e2def489ba377a
tatui1 <kamila.lumazova@compasscollege.art>
2026-02-05 01:22:05 +0600

    исправление дз

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { Watchlist } from "./components/Watchlist/Watchlist";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div className="App">
+      <Watchlist />
+    </div>
   )
 }
 
