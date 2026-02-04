ba0b122c25388fc8503c0d7876b9b7dab2e1b945
tatui1 <kamila.lumazova@compasscollege.art>
2026-02-05 01:18:24 +0600

    исправление дз

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
 import './App.css'
-import { Todo } from "./components/Todo/Todo";
 
 function App() {
+  const [count, setCount] = useState(0)
 
   return (
-    <div>
-      <Todo/>
-    </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
   )
 }
 
