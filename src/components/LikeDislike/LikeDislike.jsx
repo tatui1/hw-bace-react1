c08744973d1ce1212012e2249058ccbff594bd3f
tatui1 <kamila.lumazova@compasscollege.art>
2026-02-07 16:29:39 +0600

    создаем новый файл LikeDislike

---
+++ src/components/LikeDislike/LikeDislike.jsx
@@ -0,0 +1,12 @@
+export const LikeDislike = ({ reaction, onLike, onDislike }) => {
+  return (
+    <div>
+      <button
+        onClick={onLike}
+        style={{ color: reaction === "like" ? "green" : "black" }}>Like</button>
+      <button
+        onClick={onDislike}
+        style={{ color: reaction === "dislike" ? "red" : "black" }}>Dislike</button>
+    </div>
+  )
+}
