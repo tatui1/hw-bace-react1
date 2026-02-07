043ec6d1570c8e934e0cb250a77e0324184e2066
tatui1 <kamila.lumazova@compasscollege.art>
2026-02-07 16:38:55 +0600

    добавили реакцию и новые функции

--- src/components/Watchlist/Watchlist.jsx
+++ src/components/Watchlist/Watchlist.jsx
@@ -1,27 +1,56 @@
 import { useState } from "react"
-import { AddMovie } from "../OnAddMovie/OnAddMovie"
+import { AddMovie } from "./AddMovie"
 import { MovieList } from "../MovieList/MovieList"
 
 export const Watchlist = () => {
-    const [movies, setMovies] = useState([])
+  const [movies, setMovies] = useState([])
 
-    const onAddMovie = (movieTitle) => {
-        const newMessage = {
-            id: Date.now(),
-            title: movieTitle
-        }
-        setMovies([...movies, newMessage])
+  const addMovie = (title) => {
+    const newMovie = {
+      id: Date.now(),
+      title,
+      watched: false,
+      reaction: null
     }
+    setMovies([...movies, newMovie])
+  }
 
-    const onDeleteMovie = (id) => {
-        setMovies(movies.filter(movie => movie.id !== id))
-    }
+  const onDeleteMovie = (id) => {
+    setMovies(movies.filter(movie => movie.id !== id))
+  }
 
-    return (
-        <div className="container"> 
-            <h1>To watch list:</h1>
-            <AddMovie onAddMovie={onAddMovie}/>
-            <MovieList movies={movies} onDeleteMovie={onDeleteMovie}/>  
-        </div>
+  const toggleWatched = (id) => {
+    setMovies(
+      movies.map(movie =>
+        movie.id === id
+          ? { ...movie, watched: !movie.watched, reaction: null }
+          : movie
+      )
     )
+  }
+
+  const setReaction = (id, reaction) => {
+    setMovies(
+      movies.map(movie =>
+        movie.id === id
+          ? { ...movie, reaction }
+          : movie
+      )
+    )
+  }
+
+  return (
+    <div className="container">
+      <h1>To watch list</h1>
+
+      <AddMovie onAddMovie={addMovie} />
+
+      <MovieList
+        movies={movies}
+        onDeleteMovie={onDeleteMovie}
+        toggleWatched={toggleWatched}
+        setReaction={setReaction}
+      />
+    </div>
+  )
 }
